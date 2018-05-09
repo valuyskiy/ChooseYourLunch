b10514f192e1e95e9321f3310d7ff5653a349dac
Alexandr Valuyskiy <alexandr@valuyskiy.ru>
2018-05-09 13:12:25 +0300

    Add User service

---
+++ src/main/java/ru/valuyskiy/chooseyourlunch/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package ru.valuyskiy.chooseyourlunch.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import ru.valuyskiy.chooseyourlunch.model.User;
+import ru.valuyskiy.chooseyourlunch.repository.UserRepository;
+import ru.valuyskiy.chooseyourlunch.util.exception.NotFoundException;
+
+import java.util.List;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User get(int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User getByEmail(String email) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void update(User user) {
+    }
+
+    @Override
+    public User create(User user) {
+        Assert.notNull(user, "user must not be null");
+        return repository.save(user);
+    }
+
+    @Override
+    public void delete(int id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return repository.findAll();
+    }
+
+}
