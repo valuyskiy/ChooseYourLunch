c2cabb9f54bb92a90ea5bc2bb94d8c6bf8ae924f
Alexandr Valuyskiy <alexandr@valuyskiy.ru>
2018-05-09 16:47:46 +0300

    Update model Restaurant

--- src/main/java/ru/valuyskiy/chooseyourlunch/model/Restaurant.java
+++ src/main/java/ru/valuyskiy/chooseyourlunch/model/Restaurant.java
@@ -1,4 +1,30 @@
 package ru.valuyskiy.chooseyourlunch.model;
 
-public class Restaurant {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "restaurants", uniqueConstraints = @UniqueConstraint(columnNames = "name", name = "restaurant_unique_name_idx"))
+public class Restaurant extends AbstractNamedEntity{
+
+    public Restaurant() {
+    }
+
+    public Restaurant(Integer id, String name) {
+        super(id, name);
+    }
+
+    public Restaurant(String name) {
+        super(null, name);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
