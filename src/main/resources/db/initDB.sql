5da1f8dca5f8e8f28e723663ddfafc7bd83f1cf6
Alexandr Valuyskiy <alexandr@valuyskiy.ru>
2018-05-09 14:46:50 +0300

    Update DB SQL scripts

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 
 DROP SEQUENCE IF EXISTS global_seq;
@@ -11,3 +12,11 @@ CREATE TABLE users
   password         VARCHAR                 NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE user_roles
+(
+  user_id INTEGER NOT NULL,
+  role    VARCHAR,
+  CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
