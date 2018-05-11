34b8ec6194840df59e684f620693cbdb23cf6026
Alexandr Valuyskiy <alexandr@valuyskiy.ru>
2018-05-11 20:41:40 +0300

    Update DB initialisation

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -9,19 +9,53 @@ DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 CREATE SEQUENCE global_seq START 100000;
 
--- CREATE TABLE users
--- (
---   id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
---   name             VARCHAR                 NOT NULL,
---   email            VARCHAR                 NOT NULL,
---   password         VARCHAR                 NOT NULL
--- );
--- CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
---
--- CREATE TABLE user_roles
--- (
---   user_id INTEGER NOT NULL,
---   role    VARCHAR,
---   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
---   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
--- );
+CREATE TABLE users
+(
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name             VARCHAR                 NOT NULL,
+  email            VARCHAR                 NOT NULL,
+  password         VARCHAR                 NOT NULL
+);
+CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE user_roles
+(
+  user_id         INTEGER                 NOT NULL,
+  role            VARCHAR,
+  CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) MATCH FULL ON DELETE CASCADE
+);
+
+CREATE TABLE restaurants(
+  id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name            VARCHAR                 NOT NULL
+);
+CREATE UNIQUE INDEX restaurants_unique_name_idx ON users (name);
+
+CREATE TABLE menu(
+  id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  date            DATE                    NOT NULL,
+  restaurant_id   INTEGER                 NOT NULL,
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (id) MATCH FULL ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX menu_unique_restaurant_date_idx ON menu (restaurant_id, date);
+
+CREATE TABLE dishes(
+  id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name            VARCHAR                 NOT NULL,
+  price           INTEGER                 NOT NULL,
+  menu_id         INTEGER                 NOT NULL,
+  FOREIGN KEY (menu_id) REFERENCES menu (id) MATCH FULL ON DELETE CASCADE,
+  CONSTRAINT dishes_price_check CHECK (price <= 1000000 AND price >= 0)
+);
+CREATE UNIQUE INDEX dishes_unique_menu_name_idx ON dishes (menu_id, name);
+
+
+CREATE TABLE vote(
+  id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  menu_id         INTEGER                 NOT NULL,
+  user_id         INTEGER                 NOT NULL,
+  FOREIGN KEY (menu_id) REFERENCES public.menu (id) MATCH FULL ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES public.users (id) MATCH FULL ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX vote_unique_menu_user_idx ON vote (menu_id, user_id);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -3,17 +3,19 @@ DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
-  ('Admin', 'admin@gmail.com', 'admin'),
-  ('Администратор', 'admin1@mail.ru', 'admin'),
-  ('User', 'user@google.ru', 'password'),
-  ('Пользователь', 'user1@yandex.ru', 'password'),
-  ('User 3', 'user3@yahoo.com', 'password');
+  ('Admin', 'admin@gmail.com', 'adminPassword'),
+  ('User', 'user@google.ru', 'userPassword');
 
+--   ('Администратор', 'admin1@mail.ru', 'admin'),
+--   ('User', 'user@google.ru', 'userPassword'),
+--   ('Пользователь', 'user1@yandex.ru', 'password'),
+--   ('User 3', 'user3@yahoo.com', 'password3');
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_ADMIN', 100000),
-  ('ROLE_ADMIN', 100001),
-  ('ROLE_USER', 100001),
-  ('ROLE_USER', 100002),
-  ('ROLE_USER', 100003),
-  ('ROLE_USER', 100004);
+  ('ROLE_USER', 100000),
+  ('ROLE_USER', 100001);
+
+--   ('ROLE_USER', 100002),
+--   ('ROLE_USER', 100003),
+--   ('ROLE_USER', 100004);
